b29ca9929b61ea37a522fb970bca00cb682fdd41
bruyer_k <bruyer_k@epitech.eu>
2014-03-05 18:25:44 -0800

    c'est con d'oublier un fichier :/

---
+++ Streameus/DataAbstractionLayer/Initializers/StreameusInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Streameus.Models;
+
+namespace Streameus.DataAbstractionLayer.Initializers
+{
+    public class StreameusInitializer : DropCreateDatabaseAlways<StreameusContainer>
+    {
+        protected override void Seed(StreameusContainer context)
+        {
+            StreameusSeeder.Seed(context);
+        }
+    }
+}

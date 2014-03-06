5ad5393544ebb05ba865ced53c320e377f3b0faf
bruyer_k <bruyer_k@epitech.eu>
2014-03-05 20:32:00 -0800

    Il semblerait qu'un bloc ligne parasite dans le web.config soit la source de tous mes problemes.

--- Streameus/DataAbstractionLayer/Initializers/DropCreateDatabaseTables.cs
+++ Streameus/DataAbstractionLayer/Initializers/DropCreateDatabaseTables.cs
@@ -27,8 +27,9 @@ namespace Streameus.DataAbstractionLayer.Initializers
             if (dbExists)
             {
                 // remove all tables
+                context.Database.ExecuteSqlCommand(
+                    "DECLARE @table_schema varchar(100) ,@table_name varchar(100) ,@constraint_schema varchar(100) ,@constraint_name varchar(100) ,@cmd nvarchar(200) DECLARE constraint_cursor CURSOR FOR select CONSTRAINT_SCHEMA, CONSTRAINT_NAME, TABLE_SCHEMA, TABLE_NAME from INFORMATION_SCHEMA.TABLE_CONSTRAINTS where TABLE_NAME != 'sysdiagrams' order by CONSTRAINT_TYPE asc  OPEN constraint_cursor FETCH NEXT FROM constraint_cursor INTO @constraint_schema, @constraint_name, @table_schema, @table_name WHILE @@FETCH_STATUS = 0  BEGIN SELECT @cmd = 'ALTER TABLE [' + @table_schema + '].[' + @table_name + '] DROP CONSTRAINT [' + @constraint_name + ']'     EXEC sp_executesql @cmd FETCH NEXT FROM constraint_cursor INTO @constraint_schema, @constraint_name, @table_schema, @table_name END CLOSE constraint_cursor DEALLOCATE constraint_cursor");
                 context.Database.ExecuteSqlCommand("EXEC sp_MSforeachtable @command1 = \"DROP TABLE ?\"");
-
                 // create all tables
                 var dbCreationScript = ((IObjectContextAdapter) context).ObjectContext.CreateDatabaseScript();
                 context.Database.ExecuteSqlCommand(dbCreationScript);

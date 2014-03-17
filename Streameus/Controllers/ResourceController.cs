92bfdf8dfdf36dab1c759c3a522805e749edfbd4
aital_a <aitali.anas@gmail.com>
2014-03-18 00:35:48 +1100

    STREAM-156 #time 35m #comment Controller gérant les ressources statiques avec quelques fonctions #in-progress

---
+++ Streameus/Controllers/ResourceController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Streameus.Controllers
+{
+    /// <summary>
+    /// Resource controller
+    /// </summary>
+    [RoutePrefix("api/Resource")]
+    public class ResourceController : ApiController
+    {
+        // GET api/Resource/about
+        /// <summary>
+        /// Get about content in html format
+        /// </summary>
+        /// <returns></returns>
+        [Route("about")]
+        [HttpGet]
+        public string GetAbout()
+        {
+            return "about"; //todo read about.html from Resources and return it.
+        }
+
+        // GET api/Resource/faq
+        /// <summary>
+        /// Get faq content in html format
+        /// </summary>
+        /// <returns></returns>
+        [Route("faq")]
+        [HttpGet]
+        public string GetFaq()
+        {
+            return "faq";//todo read faq.html from Resources and return it. Or maybe return a list in json.
+        }
+
+        // GET api/Resource/team
+        /// <summary>
+        /// Get team content in html format
+        /// </summary>
+        /// <returns></returns>
+        [Route("team")]
+        [HttpGet]
+        public string GetTeam()
+        {
+            return "team";//todo read team.html from Resources and return it. Or maybe return a list in json.
+        }
+
+    }
+}

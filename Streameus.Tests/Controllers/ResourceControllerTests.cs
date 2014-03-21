77a401e723e30ce8d74d02630d848cc458398589
aital_a <aitali.anas@gmail.com>
2014-03-21 14:38:49 +1100

    STREAM-158 #time 20m #comment Debut d'implementation des tests unitaires pour ResourceController

---
+++ Streameus.Tests/Controllers/ResourceControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Streameus.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Streameus.Controllers.Tests
+{
+    [TestClass()]
+    public class ResourceControllerTests
+    {
+        [TestMethod()]
+        public void GetAboutTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void GetFaqTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void GetTeamTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

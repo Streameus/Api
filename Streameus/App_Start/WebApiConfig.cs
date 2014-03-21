ab8032b7e6f36a36202bd964da015eed754d285f
bruyer_k <bruyer_k@epitech.eu>
2014-03-21 16:25:07 -0700

    OAuth fonctionnelle, manque plus qu'a enregistrer les Credentials pour que ce soit persistant

--- Streameus/App_Start/WebApiConfig.cs
+++ Streameus/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿#pragma warning disable 1591
+﻿using Microsoft.Owin.Security.OAuth;
+#pragma warning disable 1591
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,11 @@ namespace Streameus
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // Web API configuration and services
             //Permet de controler la generation de la doc XML, est utilise pour swagger
             config.SetDocumentationProvider(new XmlDocumentationProvider(
                 HttpContext.Current.Server.MapPath("~/App_Data/StreameusDocumentation.xml")));
@@ -32,7 +38,7 @@ namespace Streameus
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { Controller = "ApiHome", id = RouteParameter.Optional }
+                defaults: new {Controller = "ApiHome", id = RouteParameter.Optional}
                 );
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }

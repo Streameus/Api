32e7a144290719a4c00b2e3641b5b97b8d5a4909
avakia_a <m4ngu3@gmail.com>
2014-03-22 09:07:39 +0100

    STREAM-157 #time 1h #comment Joujou autour du projet, nothing interesting yet.

--- Streameus/Controllers/PictureController.cs
+++ Streameus/Controllers/PictureController.cs
@@ -1,13 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
+using Streameus.DataAbstractionLayer.Contracts;
 
 namespace Streameus.Controllers
 {
-    public class PictureController : ApiController
+    /// <summary>
+    /// Picture controller
+    /// </summary>
+    public class PictureController : BaseController
     {
+        private readonly IUserServices _userServices;
+        
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public PictureController(IUserServices userServices)
+        {
+            this._userServices = userServices;
+        }
+
+        // GET api/picture
+        /// <summary>
+        /// Affichage du form
+        /// </summary>
+        public void Index()
+        {
+        }
+
+        /// <summary>
+        /// PostFormData
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="UnsupportedMediaType">This type of media is not supported</exception>
+        /// <exception cref="InternalServerError">Internal Server Error</exception>
+        public Task<HttpResponseMessage> PostFormData()
+        {
+            // Check if the request contains multipart/form-data.
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            string root = HttpContext.Current.Server.MapPath("~/App_Data");
+            var provider = new MultipartFormDataStreamProvider(root);
+
+            // Read the form data and return an async task.
+            var task = Request.Content.ReadAsMultipartAsync(provider).
+                ContinueWith<HttpResponseMessage>(t =>
+                {
+                    if (t.IsFaulted || t.IsCanceled)
+                    {
+                        Request.CreateErrorResponse(HttpStatusCode.InternalServerError, t.Exception);
+                    }
+
+                    // This illustrates how to get the file names.
+                    foreach (MultipartFileData file in provider.FileData)
+                    {
+                        Trace.WriteLine(file.Headers.ContentDisposition.FileName);
+                        Trace.WriteLine("Server file path: " + file.LocalFileName);
+                    }
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                });
+
+            return task;
+        }
     }
 }

0bacb8ba888a447d4f3de3d67fed5cd4e925a44d
bruyer_k <kevyn.bruyere@epitech.eu>
2014-03-26 17:21:33 -0700

    STREAM-175 #done #time 5min #comment Suppression du fichiers parameterMap et suppression des commentaires inutiles dans StreameusContext

---
+++ Streameus/Models/StreameusContext.cs
@@ -0,0 +1,44 @@
+using System.Configuration;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using Streameus.DataAbstractionLayer.Initializers;
+using Streameus.Models.Mapping;
+
+namespace Streameus.Models
+{
+    public partial class StreameusContext : DbContext
+    {
+        static StreameusContext()
+        {
+            Database.SetInitializer<StreameusContext>(null);
+        }
+
+        public StreameusContext()
+            : base("Name=StreameusContext")
+        {
+            //Un initializer de db different est requis pour appHarbor, cf doc de la classe.
+            var appHarbor = ConfigurationManager.AppSettings["Environment"] == "AppHarbor";
+            if (appHarbor)
+                Database.SetInitializer(new StreameusInitializerForAppHarbor());
+            else
+                Database.SetInitializer(new StreameusInitializer());
+        }
+
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<ConferenceParameters> ConferenceParameters { get; set; }
+        public DbSet<Conference> Conferences { get; set; }
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new CommentMap());
+            modelBuilder.Configurations.Add(new ConferenceParametersMap());
+            modelBuilder.Configurations.Add(new DocumentMap());
+            modelBuilder.Configurations.Add(new PostMap());
+            modelBuilder.Configurations.Add(new UserMap());
+            modelBuilder.Configurations.Add(new ConferenceMap());
+        }
+    }
+}

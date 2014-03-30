1cacb2ad5f4751f0445eaadf830a7d059fda4833
avakia_a <m4ngu3@gmail.com>
2014-03-30 07:09:58 +0200

    STREAM-189 #time 20m #comment Conference.cs #done

--- Streameus/DataAbstractionLayer/DataModels/Conference.cs
+++ Streameus/DataAbstractionLayer/DataModels/Conference.cs
@@ -11,27 +11,69 @@ using Streameus.Enums;
 
 namespace Streameus.Models
 {
+    /// <summary>
+    /// Conference
+    /// </summary>
     public partial class Conference
     {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
         public Conference()
         {
             this.Documents = new HashSet<Document>();
             this.Members = new HashSet<User>();
         }
 
+        /// <summary>
+        /// Conference ID
+        /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// Conference's owner ID
+        /// </summary>
         public int OwnerId { get; set; }
+        /// <summary>
+        /// Conference name
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// Conference Description
+        /// </summary>
         public string Description { get; set; }
+        /// <summary>
+        /// Conference Status
+        /// </summary>
         public Enums.DataBaseEnums.ConfStatus Status { get; set; }
+        /// <summary>
+        /// Start Date and Time for the Conference
+        /// </summary>
         public System.DateTime Time { get; set; }
+        /// <summary>
+        /// Estimated duration for the Conference
+        /// </summary>
         public int ScheduledDuration { get; set; }
+        /// <summary>
+        /// Final duration for the Conference (set at the end)
+        /// </summary>
         public int FinalDuration { get; set; }
 
-        public virtual ConferenceParameters ConferenceParameters { get; set; }
 
+        /// <summary>
+        /// Conference Parameters
+        /// </summary>
+        public virtual ConferenceParameters ConferenceParameters { get; set; }
+        /// <summary>
+        /// Collection of Documents usefull for the Conference
+        /// </summary>
         public virtual ICollection<Document> Documents { get; set; }
+        /// <summary>
+        /// Conference Owner
+        /// </summary>
         public virtual User Owner { get; set; }
+        /// <summary>
+        /// Users participating to the Conference
+        /// </summary>
         public virtual ICollection<User> Members { get; set; }
     }
 }

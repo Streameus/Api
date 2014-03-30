62d9068ee79056b90fdc6062cc8638e7193da89e
avakia_a <m4ngu3@gmail.com>
2014-03-30 06:25:44 +0200

    STREAM-157 #time 1h #comment Method Get fonctionnel (Picture)

--- Streameus/Controllers/PictureController.cs
+++ Streameus/Controllers/PictureController.cs
@@ -30,6 +30,48 @@ namespace Streameus.Controllers
             this._userServices = userServices;
         }
 
+        // GET api/picture/{id}
+        /// <summary>
+        /// Get Picture from id
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="ApiController.NotFound">Picture not found</exception>
+        public HttpResponseMessage Get(int id)
+        {
+            var root = HttpContext.Current.Server.MapPath("~/App_Data/Picture/");
+            var file = root + id;
+            var path = "";
+            var img = new Image();
+            var type = "";
+
+            if (File.Exists(file + ".jpg"))
+            {
+                img.ImageUrl = file + ".jpg";
+                path = file + ".jpg";
+                type = "jpeg";
+            }
+            else if (File.Exists(file + ".png"))
+            {
+                img.ImageUrl = file + ".png";
+                path = file + ".png";
+                type = "png";
+            }
+            else
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            var fileStream = new FileStream(path, FileMode.Open);
+
+
+            return new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content =
+                    new StreamContent(fileStream)
+                    {
+                        Headers = {ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("image/" + type)}
+                    }
+            };
+        }
+
         // POST api/picture
         /// <summary>
         /// PostFormData
@@ -37,6 +79,8 @@ namespace Streameus.Controllers
         /// <returns></returns>
         /// <exception cref="UnsupportedMediaType">This type of media is not supported</exception>
         /// <exception cref="InternalServerError">Internal Server Error</exception>
+        /// <exception cref="FileNotFoundException">File Not Found</exception>
+        /// <exception cref="IOException">File Not Found</exception>
         public Task<HttpResponseMessage> PostFormData()
         {
             // Check if the request contains multipart/form-data.
@@ -64,9 +108,9 @@ namespace Streameus.Controllers
                         var oldfileName = file.LocalFileName;
                         // recup le userId
                         int userId = 1;
-                        string fileExtension = null;
 
                         // Recup l'extension de fichier
+                        string fileExtension = null;
                         switch (file.Headers.ContentType.MediaType)
                         {
                             case "image/png":
@@ -111,3 +155,4 @@ namespace Streameus.Controllers
         }
     }
 }
+

982756f245578df6bdd5f1eab8ae94ced5328ff0
Luca GOUTY <lucas.gouty@gmail.com>
2014-05-08 09:00:59 +0200

    STREAM-262 #inprogress #time 1h 30m #comment Sorting & Filtering messages + Check user + Ajout package OData

--- Streameus/Controllers/MessageController.cs
+++ Streameus/Controllers/MessageController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.OData.Query;
 using AutoMapper;
 using Microsoft.AspNet.Identity;
 using Streameus.DataAbstractionLayer.Contracts;
@@ -46,26 +47,29 @@ namespace Streameus.Controllers
         /// <summary>
         /// Get all the message groups
         /// </summary>
+        /// <param name="options">options for sorting and filtering</param>
         /// <returns></returns>
         /// <exception cref="NoResultException"></exception>
         [Route("Group")]
-        public IEnumerable<MessageGroupViewModel> GetGroups()
+        public IEnumerable<MessageGroupViewModel> GetGroups(ODataQueryOptions<MessageGroup> options)
         {
             var messageGroupList = new List<MessageGroupViewModel>();
-            this._messageGroupServices.GetAll().ForEach(u => messageGroupList.Add(new MessageGroupViewModel(u)));
+            var groups = options.ApplyTo(this._messageGroupServices.GetAll().AsQueryable()) as IQueryable<MessageGroup>;
+            groups.ForEach(u => messageGroupList.Add(new MessageGroupViewModel(u)));
             return messageGroupList;
         }
 
         /// <summary>
         /// Return the currently connected user message groups
         /// </summary>
+        /// <param name="options">options for sorting and filtering</param>
         /// <returns></returns>
         [Route("Group/My")]
         [Authorize]
-        public IEnumerable<MessageGroupViewModel> GetMy()
+        public IEnumerable<MessageGroupViewModel> GetMy(ODataQueryOptions<MessageGroup> options)
         {
             var userId = Convert.ToInt32(this.User.Identity.GetUserId());
-            var userGroups = this._messageGroupServices.GetAll().Where(g => g.Members.Any(m => m.Id == userId));
+            var userGroups = options.ApplyTo(this._messageGroupServices.GetAll().Where(g => g.Members.Any(m => m.Id == userId)).AsQueryable()) as IQueryable<MessageGroup>;
             var messageGroupList = new List<MessageGroupViewModel>();
             userGroups.ForEach(u => messageGroupList.Add(new MessageGroupViewModel(u, userId)));
             return messageGroupList.OrderByDescending(i => i.Date);
@@ -76,24 +80,33 @@ namespace Streameus.Controllers
         /// Get one messageGroup
         /// </summary>
         /// <param name="id">the id of the messageGroup to get</param>
+        /// <param name="options">options for sorting and filtering</param>
         /// <returns></returns>
         [Authorize]
         [Route("Group/{id}")]
-        public MessageGroupViewModel GetGroup(int id)
+        public MessageGroupViewModel GetGroup(int id, ODataQueryOptions<Message> options)
         {
             var messageGroup = this._messageGroupServices.GetById(id);
+            this.CheckUser(messageGroup);
+            var sortedMessages = options.ApplyTo(messageGroup.Messages.AsQueryable()) as IQueryable<Message>;
+            if (sortedMessages != null)
+                messageGroup.Messages = sortedMessages.ToArray();
             return new MessageGroupViewModel(messageGroup);
         }
 
         /// <summary>
         /// Get all messages of a group
         /// </summary>
-        /// <param name="id"></param>
+        /// <param name="id">id of the message group</param>
+        /// <param name="options">options for sorting and filtering</param>
         /// <returns></returns>
         [Route("{id}")]
-        public IEnumerable<MessageViewModel> GetMessages(int id)
+        public IEnumerable<MessageViewModel> GetMessages(int id, ODataQueryOptions<Message> options)
         {
-            var messages = this._messageGroupServices.GetById(id).Messages;
+            var group = this._messageGroupServices.GetById(id);
+            this.CheckUser(group);
+            var messages = group.Messages.AsQueryable();
+            messages = options.ApplyTo(messages) as IQueryable<Message>;
             var messagesList = new List<MessageViewModel>();
             messages.ForEach(m => messagesList.Add(new MessageViewModel(m)));
             return messagesList;
@@ -125,6 +138,7 @@ namespace Streameus.Controllers
                 }
                 this._messageGroupServices.AddMessageGroup(msgGroup);
             }
+            this.CheckUser(msgGroup);
             var sender = _userServices.GetById(Convert.ToInt32(this.User.Identity.GetUserId()));
             var msg = new Message
             {
@@ -142,9 +156,10 @@ namespace Streameus.Controllers
         /// Get or create a message group with users ids and return existing messages
         /// </summary>
         /// <param name="userIds">Array of users in the message group</param>
+        /// <param name="options">options for sorting and filtering</param>
         /// <returns></returns>
         [Route("Group")]
-        public NewMessageGroupViewModel Post(int[] userIds)
+        public NewMessageGroupViewModel Post(int[] userIds, ODataQueryOptions<Message> options)
         {
             var userId = Convert.ToInt32(this.User.Identity.GetUserId());
             var users = userIds.Select(id => this._userServices.GetById(id)).ToList();
@@ -154,7 +169,13 @@ namespace Streameus.Controllers
                 var existingGroup = messageGroups.SingleOrDefault(g => g.Members.Intersect(users).Count() == users.Count());
                 // Search if a group already exists with those users
                 if (existingGroup != null)
+                {
+                    // Sorting messages
+                    var sortedMessages = options.ApplyTo(existingGroup.Messages.AsQueryable()) as IQueryable<Message>;
+                    if (sortedMessages != null)
+                        existingGroup.Messages = sortedMessages.ToArray();
                     return new NewMessageGroupViewModel(existingGroup, userId);
+                }
                 // Create a new one
                 var group = new MessageGroup { Members = users };
                 this._messageGroupServices.AddMessageGroup(group);
@@ -164,6 +185,18 @@ namespace Streameus.Controllers
             {
                 throw new Exceptions.HttpErrors.NoResultException(e.Message);
             }
+        }
+
+        /// <summary>
+        /// Check if the connected user can access to this message group
+        /// </summary>
+        /// <param name="group">Message group</param>
+        private void CheckUser(MessageGroup group)
+        {
+            var userId = Convert.ToInt32(this.User.Identity.GetUserId());
+            var user = this._userServices.GetById(userId);
+            if (!group.Members.Contains(user))
+                throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.Unauthorized)); ;
         }
 
     }

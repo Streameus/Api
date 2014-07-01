569ca2bc87e01ddf746b2d8893c607dbb71f8fb8
bruyer_k <kevyn.bruyere@epitech.eu>
2014-07-01 13:29:50 +0200

    Simplification de l'utilisation des requetes authentifiees. Une fois que le token est obtenu, il suffit de le mettre dans le champs bearer token pour pouvoir faire des requetes authentifiees.

--- Streameus/Scripts/swagger.js
+++ Streameus/Scripts/swagger.js
@@ -1,3 +1,14 @@
 ﻿//$(document).ready(function () {
-    $('#header').html("<div class='swagger-ui-wrap'><a id='logo' href='/'>STREAMEUS</a></div>");
+$('#logo').html("STREAMEUS").attr('href', '/');
+$('#show-pet-store-icon').parent().next().remove();
+$('#show-pet-store-icon').parent().next().remove();
+$('#show-pet-store-icon').parent().siblings().last().remove();
+$('#show-pet-store-icon').parent().remove();
+$('#input_apiKey').change(function() {
+    var key = $('#input_apiKey')[0].value;
+    if (key && key.trim() != "") {
+        key = "bearer " + key;
+        window.authorizations.add("key", new ApiKeyAuthorization("Authorization", key, "header"));
+    }
+}).attr('placeholder', 'bearer token');
 //});

7a208bb9e3c69c77b252c3dd52ac2c662090c749
bruyer_k <kevyn.bruyere@epitech.eu>
2014-07-01 14:06:59 +0200

    Le token peut maintenant etre recupere automatiquement depuis l'url. Il est sauvegarde dans le local storage pour plus de facilite

--- Streameus/Scripts/swagger.js
+++ Streameus/Scripts/swagger.js
@@ -1,8 +1,17 @@
 ﻿//$(document).ready(function () {
+function getParameterByName(name) {
+    if (location.hash == undefined || location.hash == '')
+        return null;
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\#&!]" + name + "=([^&#!]*)"),
+        results = regex.exec(location.hash);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+
 $('#logo').html("STREAMEUS").attr('href', '/');
 $('#show-pet-store-icon').parent().next().remove();
 $('#show-pet-store-icon').parent().next().remove();
-$('#show-pet-store-icon').parent().siblings().last().remove();
 $('#show-pet-store-icon').parent().remove();
 $('#input_apiKey').change(function() {
     var key = $('#input_apiKey')[0].value;
@@ -11,4 +20,15 @@ $('#input_apiKey').change(function() {
         window.authorizations.add("key", new ApiKeyAuthorization("Authorization", key, "header"));
     }
 }).attr('placeholder', 'bearer token');
+$('#explore').html('Get token from url').click(function() {
+    var accessToken = getParameterByName('access_token');
+    if (accessToken != '' && accessToken !== undefined) {
+        $('#input_apiKey').val(accessToken).trigger('change');
+        localStorage.bearerToken = accessToken;
+    }
+    return false;
+});
+
+if (localStorage.getItem('bearerToken'))
+    $('#input_apiKey').val(localStorage.getItem('bearerToken')).trigger('change');
 //});

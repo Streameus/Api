428e85e810c36d444071313dd6252f84141dfed2
bruyer_k <kevyn.bruyere@epitech.eu>
2014-07-10 12:29:11 +0200

    Ajout d'une methode Agenda/Notifications

---
+++ Streameus/ViewModels/NotificationsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Streameus.ViewModels
+{
+    /// <summary>
+    /// The ViewModel used to return all the notifications
+    /// </summary>
+    public class NotificationsViewModel
+    {
+        /// <summary>
+        /// The user unread messages count
+        /// </summary>
+        public int UnreadMessages { get; set; }
+
+        /// <summary>
+        /// All the conferences the user suscribed to which are live
+        /// </summary>
+        public IEnumerable<ConferenceAgendaViewModel> Live { get; set; }
+
+        /// <summary>
+        /// All the conferences the user suscribed to happening in the next 24h
+        /// </summary>
+        public IEnumerable<ConferenceAgendaViewModel> Soon { get; set; }
+    }
+}

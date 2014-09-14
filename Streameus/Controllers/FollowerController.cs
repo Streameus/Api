2e2bf30978fff80611eda532dea6557e611e6da8
charou_p <charou_p@epitech.eu>
2014-09-14 13:08:06 +0200

    Scroll infini sur les followers et les following

--- Streameus/Controllers/FollowerController.cs
+++ Streameus/Controllers/FollowerController.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.OData.Query;
 using Streameus.DataAbstractionLayer.Contracts;
 using Streameus.Exceptions;
 using Streameus.Exceptions.HttpErrors;
+using Streameus.Models;
 using Streameus.ViewModels;
 using WebGrease.Css.Extensions;
 using NoResultException = Streameus.Exceptions.NoResultException;
@@ -35,17 +37,21 @@ namespace Streameus.Controllers
         /// Get all the followers for an user
         /// </summary>
         /// <param name="id">The user id</param>
+        /// <param name="options">OData query options</param>
         /// <exception cref="Exceptions.HttpErrors.NoResultException">If the user doesn't have any followers</exception>
         /// <returns></returns>
         /// <exception cref="NoResultException">If the user doesn't have any followers</exception>
         /// <exception cref="NotFoundException">If the user doesn't exists</exception>
         [Authorize]
-        public IEnumerable<UserViewModel> Get(int id)
+        public IEnumerable<UserViewModel> Get(int id, ODataQueryOptions<User> options = null)
         {
             var followersViewModels = new List<UserViewModel>();
             try
             {
                 var followers = this._userServices.GetFollowersForUser(id);
+                if (options != null)
+                    followers = options.ApplyTo(followers) as IQueryable<User>;
+
                 followers.ForEach(f => followersViewModels.Add(new UserViewModel(f)));
             }
             catch (EmptyResultException)
